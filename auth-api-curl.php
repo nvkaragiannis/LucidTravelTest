9c8a9b9e313f719beb0d24e939c881bb6d7793b7
chawlasourabh1 <chawla.sourabh1@gmail.com>
2021-05-21 16:11:39 +0530

    Update auth-api-curl.php

--- auth-api-curl.php
+++ auth-api-curl.php
@@ -3,7 +3,7 @@
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://www.lucidtravel.us/league_apps/api/league-apps-api.php");
 curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $_REQUEST);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $_EVN);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($curl);
 if (curl_errno($curl)) {
