61b05c7efaac7cc29cf62f43bb215549e49f4471
chawlasourabh1 <chawla.sourabh1@gmail.com>
2021-05-21 23:27:05 +0530

    Update auth-api-curl.php

--- auth-api-curl.php
+++ auth-api-curl.php
@@ -1,18 +1,18 @@
 <?php
-echo '<pre>'; print_r($_ENV);
+//echo '<pre>'; print_r($_ENV);
     
-$arr = [
+/*$arr = [
 'requestData' => json_encode($_REQUEST),
 'getEnvData' => json_encode(getenv()),
 'envData' => json_encode($_ENV),
 'serverData' => json_encode($_SERVER),
 'fileContent' => json_encode(file_get_contents('php://input'))
-];
+];*/
 
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://www.lucidtravel.us/league_apps/api/league-apps-api.php");
 curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $arr);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $_ENV);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($curl);
 if (curl_errno($curl)) {
@@ -21,6 +21,6 @@ if (curl_errno($curl)) {
 curl_close($curl);
 //return $data;
 
-echo '<pre>'; print_r($_ENV);
+//echo '<pre>'; print_r($_ENV);
 ?>
     
