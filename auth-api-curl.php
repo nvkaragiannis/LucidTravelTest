fc1f97ca46eae5b425a3afca55bba1c25d49243b
nvkaragiannis <50931554+nvkaragiannis@users.noreply.github.com>
2024-06-24 13:21:17 -0400

    Update auth-api-curl.php
    changing url to dev server

--- auth-api-curl.php
+++ auth-api-curl.php
@@ -10,7 +10,7 @@
 ];*/
 
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://www.lucidtravel.us/league_apps/api/league-apps-api.php");
+curl_setopt($curl, CURLOPT_URL, "https://dev.lucidtravel.us/league_apps/api/league-apps-api.php");
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $_ENV);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
