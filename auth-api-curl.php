a2ce8ba8176f386b31d70f1ce71c48ce3cfff822
rohit-datafortune <rohit.laddha@datafortune.com>
2024-06-26 20:15:38 +0530

    echoing the result to check in pandium log

--- auth-api-curl.php
+++ auth-api-curl.php
@@ -15,6 +15,8 @@ curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $_ENV);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($curl);
+
+echo $data; die;
 if (curl_errno($curl)) {
     echo 'Error:' . curl_error($curl);
 }
