a1db71ad414b9f90bb36a249c85643d8acb18abf
rohit-datafortune <rohit.laddha@datafortune.com>
2024-08-28 17:44:50 +0530

    Update auth-api-curl.php
    Returning data to see in pandium log

--- auth-api-curl.php
+++ auth-api-curl.php
@@ -11,15 +11,15 @@ echo '<pre>'; print_r($_ENV);
 // $username = 'admin';
 // $password = 'Secure@1234#';
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://www.lucidtravel.com/league_apps/api/league-apps-api.php");
+curl_setopt($curl, CURLOPT_URL, "https://dev.lucidtravel.com/league_apps/api/league-apps-api.php");
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $_ENV);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 // curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
 // curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 $data = curl_exec($curl);
-
-echo $data; die;
+echo "DATA - <br>";
+return $data;
 if (curl_errno($curl)) {
     echo 'Error:' . curl_error($curl);
 }
