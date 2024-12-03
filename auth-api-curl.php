a7fde9590cdfee3f3be7db970a25b4cae1ab919d
rohit-datafortune <rohit.laddha@datafortune.com>
2024-12-03 11:59:03 +0530

    Update auth-api-curl.php
    Updating url to point to production server

--- auth-api-curl.php
+++ auth-api-curl.php
@@ -11,7 +11,7 @@ echo '<pre>'; print_r($_ENV);
 // $username = 'admin';
 // $password = 'Secure@1234#';
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, "https://dev.lucidtravel.com/league_apps/api/league-apps-api.php");
+curl_setopt($curl, CURLOPT_URL, "https://lucidtravel.com/league_apps/api/league-apps-api.php");
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $_ENV);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -19,8 +19,7 @@ curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 // curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
 // curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 $data = curl_exec($curl);
-echo "DATA - <br>";
-echo $data;
+echo "DATA - ".$data;
 if (curl_errno($curl)) {
     echo 'Error:' . curl_error($curl);
 }
