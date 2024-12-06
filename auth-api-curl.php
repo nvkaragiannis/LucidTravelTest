c4ec7e4ccb9d22f4d29aa72391cb12061da9b6fb
rohit-datafortune <rohit.laddha@datafortune.com>
2024-12-06 17:17:36 +0530

    Update auth-api-curl.php
    Added username password to access dev server

--- auth-api-curl.php
+++ auth-api-curl.php
@@ -8,16 +8,16 @@ echo '<pre>'; print_r($_ENV);
 'serverData' => json_encode($_SERVER),
 'fileContent' => json_encode(file_get_contents('php://input'))
 ];*/
-// $username = 'admin';
-// $password = 'Secure@1234#';
+$username = 'admin';
+$password = 'Secure@1234#';
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://192.168.21.31/league_apps/api/league-apps-api.php");
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $_ENV);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-// curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
-// curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
+curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 $data = curl_exec($curl);
 echo "DATA - ".$data;
 if (curl_errno($curl)) {
